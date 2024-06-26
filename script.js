bd3e6ffb68a4f4fc34ef7502623c2dab9f87445e
Najeeba <najeebaqarqin@gmail.com>
2024-06-27 00:16:40 +0430

    Added variable to assign the roman and number arrays

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const number = document.querySelector('#number');
 const button = document.querySelector('#convert-btn');
 const result = document.querySelector('#output');
 let value = number.value;
+
+const numeral = [
+  ['M', 1000],
+  ['CM', 900],
+  ['D', 500],
+  ['CD', 400],
+  ['C', 100],
+  ['XC', 90],
+  ['L', 50],
+  ['XL', 40],
+  ['X', 10],
+  ['IX', 9],
+  ['V', 5],
+  ['IV', 4],
+  ['I', 1]
+];
