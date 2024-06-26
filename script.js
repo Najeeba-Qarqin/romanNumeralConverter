f386887150f126645682cbfdfde77873c6ddc508
Najeeba <najeebaqarqin@gmail.com>
2024-06-27 00:10:26 +0430

    Declared variables for input button and result div

---
+++ script.js
@@ -0,0 +1,4 @@
+const number = document.querySelector('#number');
+const button = document.querySelector('#convert-btn');
+const result = document.querySelector('#output');
+let value = number.value;
